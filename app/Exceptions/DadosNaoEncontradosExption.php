73c3d61226fb3cb3a2d814bc7e3d64c6d39d2e2c
Fernando Nascimento <ln330011@gmail.com>
2022-11-30 18:26:29 -0300

    v.02

---
+++ app/Exceptions/DadosNaoEncontradosExption.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class DadosNaoEncontradosExption
+{
+    public $message;
+
+    public function ShowMessage()
+    {
+        
+    }
+}
